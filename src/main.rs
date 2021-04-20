7e561fbfea2144590c40d92dfff4e272556bb500
HenryZou1 <henryzou2@hotmail.com>
2021-04-19 23:28:19 -0400

    v1

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+use rand::Rng;
+
+
+fn main() {
+	let mut number: u8 = 0;
+	let mut num = magic_num::magic_number();
+	//8th bit
+	number = number | num;
+	number = number << 1;
+	num = magic_num::magic_number();
+	//7
+	number = number | num;
+	number = number << 1;
+	num = magic_num::magic_number();
+	//6
+	number = number | num;
+	number = number << 1;
+	num = magic_num::magic_number();
+	//5
+	number = number | num;
+	number = number << 1;
+	num = magic_num::magic_number();
+	//4
+	number = number | num;
+	number = number << 1;
+	num = magic_num::magic_number();
+	//3
+	number = number | num;
+	number = number << 1;
+	num = magic_num::magic_number();
+	//2
+	number = number | num;
+	number = number << 1;
+	num = magic_num::magic_number();
+	//1
+	number = number | num;
+    println!("{}",number);
+	number = rand::thread_rng().gen_range(0, 231) | num;
+	println!("{}",number);
+}
